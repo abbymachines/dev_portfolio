752b42f0101a1eba93821ad76e6f9805a9e9758f
abbymachines <abby.machines@gmail.com>
2023-09-06 15:52:09 -0700

    implement value increment & decrement buttons, as way to introduce state to my app

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,27 @@
 import './Card.css';
+import {useState} from 'react';
 
 const Card = ({shortText, extraText, image}) => {
+  const [cardNum, setCardNum] = useState(0);
+
+  const increaseCard = () => {
+    setCardNum(cardNum + 1);
+    console.log(cardNum);
+  };
+
+  const decreaseCard = () => {
+    setCardNum(cardNum - 1);
+    console.log(cardNum);
+  }
+
   return (
   <section className="card-box">
     <p>{shortText}</p>
     <p>{extraText}</p>
     <p>{image}</p>
+    <p>{cardNum}</p>
+    <button onClick={increaseCard}>pump it up</button>
+    <button onClick={decreaseCard}>turn it down</button>
   </section>)
 }
 
