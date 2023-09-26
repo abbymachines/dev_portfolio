a594719959a7c9314fc2048b6d27a634601941cf
abbymachines <abby.machines@gmail.com>
2023-09-25 18:41:35 -0700

    added center styling, but now the cards are ordered horizontally lol

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,22 +4,25 @@ function Cards(props) {
   const cards = props.cardsData;
 
   return (
-    <div>
+    <div className="center">
+      <p>
       <Card 
         mainText={cards[0].mainText}
         extraText={cards[0].extraText}
         image={cards[0].image}
-      />
+      /> </p>
+      <p>
       <Card 
         mainText={cards[1].mainText}
         extraText={cards[1].extraText}
         image={cards[1].image}
-      />
+      /> </p>
+      <p>
       <Card 
         mainText={cards[2].mainText}
         extraText={cards[2].extraText}
         image={cards[2].image}
-      />
+      /> </p>
     </div>
   )
 }
