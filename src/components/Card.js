5bf0d5cab2dc0e8f3e8b38d8cba2da2d7e953759
abbymachines <abby.machines@gmail.com>
2023-09-28 13:13:40 -0700

    refactor Card CSS style into a variable (for callback reassignment)

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,18 @@ import './Card.css';
 import ExpandButton from './ExpandButton';
 
 function Card(props) {
+  const cardStyle = 'card-box center';
+
   function expandCard() {
     console.log('this button will expand the card when i figure out how to do that');
   }
 
   return (
-    <div className='card-box center'>
+    <div className={cardStyle}>
       <p>{props.mainText}</p>
       <p>{props.extraText}</p>
       <p>{props.image}</p>
-      <ExpandButton clickFunction={expandCard} />
+      <ExpandButton clickHandler={expandCard} />
     </div>
   )
 }
