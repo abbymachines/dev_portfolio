24a2ad93fe8deabf7d1e32a4eaec8055c93f38b3
Abby Castillo <abby.machines@gmail.com>
2026-03-26 17:45:01 -0700

    adjust header styling

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
+import "./Title.css";
 
 function Title() {
   return (
-    <header>
-      <center>
-        <h1 className="website-title">
-          <Link to={"/"}>Abby Castillo</Link>
-        </h1>
-      </center>
-      <center className="website-subtitle">software engineer</center>
-      <center className="home-links">
-        <a href="mailto:abby.machines@gmail.com">email</a> /{" "}
-        <a href="https://github.com/abbymachines">GitHub</a> /{" "}
-        <a href="https://linkedin.com/in/abbycc">LinkedIn</a> /{" "}
-        <a href="https://abbymachines.art/cv">artist C.V.</a>
-      </center>
-      <center>
-        <Link to="/blog">dev blog</Link>
-      </center>
+    <header className="title-header">
+      <div className="title-content">
+        <center>
+          <h1 className="website-title">
+            <Link to={"/"}>Abby Castillo</Link>
+          </h1>
+        </center>
+        <center className="website-subtitle">software engineer</center>
+        <center className="home-links">
+          <a href="mailto:abby.machines@gmail.com">email</a> /{" "}
+          <a href="https://github.com/abbymachines">GitHub</a> /{" "}
+          <a href="https://linkedin.com/in/abbycc">LinkedIn</a> /{" "}
+          <a href="https://abbymachines.art/cv">artist C.V.</a>
+        </center>
+        <center>
+          <Link to="/blog">dev blog</Link>
+        </center>
+      </div>
     </header>
   );
 }
